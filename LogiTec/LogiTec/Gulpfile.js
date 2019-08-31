ea0545f700e8369b744b46d797419a7ec19db9d3
Lucas Melo <oliveiramelo1996@gmail.com>
2019-08-31 18:22:40 -0300

    adicionado gulp

---
+++ LogiTec/LogiTec/Gulpfile.js
@@ -0,0 +1,15 @@
+﻿const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+const resource = {
+    sass: { src: "./Assets/scss/**/*.scss", dest:'./wwwroot/css/'},
+    js: ""
+};
+gulp.task("compile:sass", () => {
+    return gulp
+        .src(resource.sass.src)
+        .pipe(sass())
+        .pipe(gulp.dest(resource.sass.dest));
+});
+
+
